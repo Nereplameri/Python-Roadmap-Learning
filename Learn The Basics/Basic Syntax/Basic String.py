db7d3093c17760ef65462a9eba99d355cbec344b
Nereplameri <alperenavci2002@gmail.com>
2025-03-10 13:12:49 +0300

    Syntax temelleri öğreniliyor

---
+++ Learn The Basics/Basic Syntax/Basic String.py
@@ -0,0 +1,8 @@
+#Strings
+my_str = "PythonPPP for Beginners"
+print(my_str.upper())
+print(my_str.find("y",1,4))
+
+print(my_str.replace("P","J", 2))
+print(my_str.find("for"))
+
---
+++ Learn The Basics/Basic Syntax/Basic calculator.py
@@ -0,0 +1,7 @@
+#Basic calculator app (sum only)
+
+numb1 = str(input("Enter your first number: "))
+numb2 = str(input("Enter your second number: "))
+sum = int(numb1) + int(numb2)
+print(sum)
+
